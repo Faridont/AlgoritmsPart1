551be68b7a3fcc2cb885a1c80a5fa3a8e60f01a1
Farik <mr.farik228@gmail.com>
2023-07-27 08:58:42 +0600

    Colinear points continue

--- BruteCollinearPoints.java
+++ BruteCollinearPoints.java
@@ -1,20 +1,77 @@
-///* *****************************************************************************
-// *  Name:              Ada Lovelace
-// *  Coursera User ID:  123456
-// *  Last modified:     October 16, 1842
-// **************************************************************************** */
-//
-// public class BruteCollinearPoints {
-//    // finds all line segments containing 4 points
-//    public BruteCollinearPoints(Point[] points) {
-//        for
-//    }
-//
-//    // the number of line segments
-//    public int numberOfSegments() {
-//    }
-//
-//    // the line segments
-//    public LineSegment[] segments() {
-//    }
-//}
+/* *****************************************************************************
+ *  Name:              Ada Lovelace
+ *  Coursera User ID:  123456
+ *  Last modified:     October 16, 1842
+ **************************************************************************** */
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BruteCollinearPoints {
+    private final Point[] points;
+    private final LineSegment[] lineSegments;
+
+    // finds all line segments containing 4 points
+    public BruteCollinearPoints(Point[] points) {
+        checkExceptions(points);
+        this.points = points;
+        this.lineSegments = getLineSegments(points);
+    }
+
+    private void checkExceptions(Point[] points) {
+        if (points == null) throw new IllegalArgumentException("Points is null");
+
+        for (int i = 0; i < points.length; i++) {
+            Point first = points[i];
+            if (first == null) throw new IllegalArgumentException("Have null point");
+
+            for (int j = 0; j < points.length; j++) {
+                if (j == i) continue;
+                Point second = points[j];
+                if (second == null) throw new IllegalArgumentException("Have null point");
+                if (first.compareTo(second) == 0)
+                    throw new IllegalArgumentException("Have compayers points");
+            }
+        }
+    }
+
+    private LineSegment[] getLineSegments(Point[] points) {
+        int N = points.length;
+        List<LineSegment> ls = new ArrayList<LineSegment>();
+        for (int i = 0; i < N; i++) {
+            Point first = points[i];
+            for (int j = 0; j < N; j++) {
+                if (j == i) continue;
+                Point second = points[j];
+                for (int k = 0; k < N; k++) {
+                    if (k == j || k == i) continue;
+                    Point third = points[k];
+                    for (int l = 0; l < N; l++) {
+                        if (l == k || l == j || l == i) continue;
+                        Point fourth = points[l];
+                        double firstSlope = first.slopeTo(second);
+                        double secondSlope = second.slopeTo(third);
+                        double thirdSlope = third.slopeTo(fourth);
+
+                        boolean isSloped = firstSlope == secondSlope && secondSlope == thirdSlope;
+                        if (isSloped) {
+                            ls.add(new LineSegment(first, fourth));
+                        }
+                    }
+                }
+            }
+        }
+
+        return (LineSegment[]) ls.toArray();
+    }
+
+    // the number of line segments
+    public int numberOfSegments() {
+        return this.lineSegments.length;
+    }
+
+    // the line segments
+    public LineSegment[] segments() {
+        return this.lineSegments;
+    }
+}
--- Test.java
+++ Test.java
@@ -5,18 +5,18 @@
  **************************************************************************** */
 
 import edu.princeton.cs.algs4.StdDraw;
+import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.StdRandom;
 
 public class Test {
     public static void main(String[] args) {
-        int XYscale = 10000;
-        int N = 5000;
+        int XYscale = 100;
+        int N = 60;
         Point[] points = new Point[N];
         // rescale coordinates and turn on animation mode
         StdDraw.setXscale(0, XYscale);
         StdDraw.setYscale(0, XYscale);
-        StdDraw.setPenRadius(0.001);  // make the points a bit larger
-
+        StdDraw.setPenRadius(0.005);  // make the points a bit larger
         for (int i = 0; i < N; i++) {
             Point p = new Point(StdRandom.uniformInt(XYscale), StdRandom.uniformInt(XYscale));
             points[i] = p;
@@ -26,12 +26,68 @@ public class Test {
             points[i].draw();
         }
 
+        // for (int i = 0; i < N; i++) {
+        //    Point p = points[i];
+        //    for (int j = 0; j < N; j++) {
+        //        if (j == i) continue;
+        //        Point p2 = points[j];
+        //        StdOut.println(
+        //                "A: " + p.toString() + " B: " + p2.toString() + " = " + p.slopeTo(p2));
+        //    }
+        //}
+
         for (int i = 0; i < N; i++) {
-            int next = i + 1;
-            if (next != N) {
-                LineSegment ls = new LineSegment(points[i], points[i + 1]);
-                ls.draw();
+            Point first = points[i];
+            for (int j = 0; j < N; j++) {
+                if (j == i) {
+                    continue;
+                }
+                Point second = points[j];
+                for (int k = 0; k < N; k++) {
+                    if (k == j || k == i) {
+                        continue;
+                    }
+                    Point third = points[k];
+                    for (int l = 0; l < N; l++) {
+                        if (l == k || l == j || l == i) {
+                            continue;
+                        }
+                        Point fourth = points[l];
+                        double firstSlope = first.slopeTo(second);
+                        double secondSlope = second.slopeTo(third);
+                        double thirdSlope = third.slopeTo(fourth);
+
+                        boolean isSloped = firstSlope == secondSlope && secondSlope == thirdSlope;
+                        if (isSloped) {
+                            first.drawTo(second);
+                            second.drawTo(third);
+                            third.drawTo(fourth);
+                        }
+                    }
+                }
             }
         }
+
+        BruteCollinearPoints bcp = new BruteCollinearPoints(points);
+        LineSegment[] ls = bcp.segments();
+        int segmentLength = bcp.numberOfSegments();
+
+        StdOut.println("Segment length: " + segmentLength);
+        for (int i = 0; i < segmentLength; i++) {
+            LineSegment ls1 = ls[i];
+            StdOut.println(ls1.toString());
+        }
+
+        StdOut.print("I am finish!!!");
+
+        StdOut.print("I am finish!!!");
+
+        // for (int i = 0; i < N; i++) {
+        //    int next = i + 1;
+        //    if (next != N) {
+        //        LineSegment ls = new LineSegment(points[i], points[i + 1]);
+        //        ls.draw();
+        //    }
+        //}
     }
 }

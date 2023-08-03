2d01a9d67a703ae5485e478aaaa0e34636a1672f
Farik <mr.farik228@gmail.com>
2023-08-03 10:06:45 +0600

    Changes in student test

---
+++ KtResults.java
@@ -0,0 +1,21 @@
+package KtTest;/* *****************************************************************************
+ *  Name:              Ada Lovelace
+ *  Coursera User ID:  123456
+ *  Last modified:     October 16, 1842
+ **************************************************************************** */
+
+
+public class KtResults {
+    int English;
+    int CritM;
+    int AnalM;
+    int Prof1;
+    int Prof2;
+
+    public KtResults() {
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
--- StudentTest.java
+++ StudentTest.java
@@ -1,4 +1,4 @@
-/* *****************************************************************************
+package KtTest;/* *****************************************************************************
  *  Name:              Ada Lovelace
  *  Coursera User ID:  123456
  *  Last modified:     October 16, 1842
@@ -15,22 +15,34 @@ public class StudentTest {
         int generationRes = 0;
         int studentCount = 4000;
         int threshold = 84;
-        int minPoint = 20;
-        int maxPoint = 151;
 
         for (int j = 0; j < countGenerations; j++) {
             HashMap<Integer, Integer> results = new HashMap<Integer, Integer>();
-            int[] studentPoint = new int[studentCount];
+            KtResults[] studentPoint = new KtResults[studentCount];
             int grantCounts = 0;
             for (int i = 0; i < studentCount; i++) {
-                studentPoint[i] = StdRandom.uniformInt(minPoint, maxPoint);
+                KtResults studentResult = new KtResults();
+                studentResult.English = StdRandom.uniformInt(0, 51);
+                studentResult.CritM = StdRandom.uniformInt(0, 16);
+                studentResult.AnalM = StdRandom.uniformInt(0, 16);
+                studentResult.Prof1 = StdRandom.uniformInt(0, 31);
+                studentResult.Prof2 = StdRandom.uniformInt(0, 41);
+                studentPoint[i] = studentResult;
             }
 
             for (int i = 0; i < studentCount; i++) {
-                int point = studentPoint[i];
-                if (point > threshold) {
+                KtResults studentRes = studentPoint[i];
+                int point = studentRes.English + studentRes.CritM + studentRes.AnalM
+                        + studentRes.Prof1 + studentRes.Prof2;
+                if (
+                        studentRes.English >= 25
+                                && (studentRes.CritM + studentRes.AnalM) >= 7
+                                && studentRes.Prof1 >= 7 && studentRes.Prof2 >= 7
+                                && point >= threshold
+                ) {
                     grantCounts++;
                 }
+
                 if (results.containsKey(point)) {
                     int curPointRes = results.get(point);
                     results.put(point, curPointRes + 1);
